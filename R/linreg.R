2ffea96b30f3de37604acca76f36a348a3f702ed
mera2 <merastat@gmail.com>
2016-09-20 11:26:34 +0200

    make few changes on the code.

--- R/linreg.R
+++ R/linreg.R
@@ -1,12 +1,18 @@
-linreg<-function(formula,data){
-  
+linreg<-function(formula,data)
+{
   x<-model.matrix(formula,data)
   y<-all.vars(formula)[1]
+  class(linreg)="linreg"
+  beta<- as.vector((solve((t(x)%*%x))%*%t(x))%*%data[,y])
+  fitted<-x%*%beta
+  resd<-as.vector(data[,y]-fitted)
+  df<- nrow(data)-length(data)
+  resvariace<- as.vector((t(resd)%*%resd)%/%df)
+  varcoef<-resvariace*(solve(t(x)%*%x))
+  tvalues<-beta/(sqrt(varcoef))
   
-  regcof<-as.vector(solve(t(x)%*%x)%*%t(x)%*%data[,y])
-  fitted_values<-x%*%regcof
-  resi<-y-fitted_values
+  return(fitted)
   
 }
 data("iris")
-linreg(Petal.Length~Petal.Width,iris)
+linreg(Petal.Length~Sepal.Width,iris)
