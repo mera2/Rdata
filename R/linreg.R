e3ac757d03dae154cab00859ca7a1bffd3cc19c2
mera2 <merastat@gmail.com>
2016-09-20 13:27:02 +0200

    costracted an object called formula under a class linearreg.

--- R/linreg.R
+++ R/linreg.R
@@ -1,18 +1,15 @@
-linreg<-function(formula,data)
+formulas<-list(formula,data)
+class(formulas)<-"lineareg"
+formulas<-function(formula,data)
 {
   x<-model.matrix(formula,data)
   y<-all.vars(formula)[1]
-  class(linreg)="linreg"
   beta<- as.vector((solve((t(x)%*%x))%*%t(x))%*%data[,y])
-  fitted<-x%*%beta
+  fitted<-as.vector(x%*%beta)
   resd<-as.vector(data[,y]-fitted)
   df<- nrow(data)-length(data)
   resvariace<- as.vector((t(resd)%*%resd)%/%df)
   varcoef<-resvariace*(solve(t(x)%*%x))
   tvalues<-beta/(sqrt(varcoef))
-  
-  return(fitted)
-  
+  return(tvalues)
 }
-data("iris")
-linreg(Petal.Length~Sepal.Width,iris)
