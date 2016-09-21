b423c9cc47e82d0ed4ee3da2f619e0ae72409437
mera2 <merastat@gmail.com>
2016-09-21 16:38:43 +0200

    creat a class called lin_reg and make some changes in the formulas

---
+++ R/lin_class.r
@@ -0,0 +1,44 @@
+lin_class <- setRefClass(
+  "lin_class",
+  fields = list(
+    formula = "character",
+    beta = "numeric",
+    fitted = "numeric",
+    resd = "numeric",
+    df = "integer",
+    resvariance = "numeric",
+    varcoef = "numeric",
+    tvalues = "numeric"),
+  methods = list(
+    print = function() {
+      cat('Call:\n','linreg(formula = ',formula[2],'~',formula[3],", data = iris",")\n\n",sep = '',"coefficients:\n\n")
+      colnames = names(beta)
+      beta
+      
+    },
+    plot = function() {
+      library ( ggplot2 )
+      
+        ggplot ( data = iris) +
+        aes(x=ls$fitted,y =ls$resd ) +
+        geom_line()
+      
+    },
+    resid = function() {
+      return(resd)
+    },
+    pred = function() {
+      return(fitted)
+    },
+    coef = function() {
+      return(resvariance) 
+    },
+    summary = function() {
+      cat('Call:\n','linreg(formula = ',formula[2],'~',formula[3],')\n\n',sep = '')
+      mat <- matrix(c(beta,varcoef, tvalues),nrow = length(beta), ncol=3,byrow=FALSE)
+      colnames(mat) <- c('Estimate', 'Std. Error', 't value')
+      rownames(mat) <- names(beta)
+     print.table(as.table(mat))
+      cat('\nResidual standard error:', resvariance, 'on', df, 'degress of freedom', sep = ' ')
+    })
+)
