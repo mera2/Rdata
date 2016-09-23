60f60b00bc2e426649aa7653dcddeb4ba4ab35de
akilahmd <aqe_85@hotmail.com>
2016-09-23 13:56:24 +0200

    commiting vignettes

--- R/linreg.R
+++ R/linreg.R
@@ -5,31 +5,29 @@ linreg<-function(formula,data)
   y<-all.vars(formula)[1]
   RC<-c((solve((t(x)%*%x))%*%t(x))%*%data[,y])
   FV<-as.vector(x%*%RC)
-  RD<-as.matrix(data[,y]-FV)
-  DF<- nrow(data)-length(data)
+  RD<-c(as.matrix(data[,y]-FV))
+  DF<- nrow(data)-ncol(x)
   RV<- c(t(RD)%*%RD)%/%DF
   VRC<-RV*(solve(t(x)%*%x))
   tvalues<-RC/sqrt(diag(VRC))
   tvalues<-round(x=tvalues,digits = 2)
-
-
- 
  
    {
   
   formulas <- lin_class$new(formula = as.character(formula),
-                                 beta=RC ,
-                                 fitted=FV, 
-                                 resd =c(RD),
-                                 df=DF,
-                                 resvariance=RV,
-                              varcoef = c(diag(VRC)),
-                              tvalues=tvalues)
-                      ls<-list(beta,fitted,resd,df,resvariance,varcoef,tvalues)
+  beta=RC ,
+  fitted=FV, 
+  resd =c(RD),
+  df=DF,
+  resvariance=RV,
+  varcoef = c(diag(VRC)),
+  tvalues=tvalues)
+                      
   }
 return(formulas)
 }
 
-
+r<-linreg(Sepal.Width~Species,iris)
+r$summary()
 
         
